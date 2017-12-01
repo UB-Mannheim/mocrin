ded42b43c908c359c5de575fcf1e3e8b6c441dbb
JKamlah <JKamlah@gmail.com>
2017-12-01 11:49:33 +0100

    renamed some variable

--- akf_mocrin.py
+++ akf_mocrin.py
@@ -5,36 +5,37 @@
 # Author:   **Jan Kamlah**
 # Date:     **30.11.2017**
 
-######### IMPORT ############
+########## IMPORT ##########
 import configparser
 from PIL import Image
 import pytesseract
 import argparse
-import cv2 as cv2
+#import cv2 as cv2
 import os
 import glob as glob2
 import subprocess
 from multiprocessing import Process
 import json
 
-####################### CMD-PARSER-SETTINGS ########################
+########## CMD-PARSER-SETTINGS ##########
 def get_args():
     """
     This function parses the cl-options
     :param:no params
     :return:the parsed cl-options
     """
-    parser = argparse.ArgumentParser()
-    #args.add_argument("-i", "--image", required=True,help="path to input image to be OCR'd")
-    #args.add_argument("-p", "--preprocess", type=str, default="thresh",help="type of preprocessing to be done")
-    parser.add_argument("--no-tess", action='store_true', help="Don't perfom tessract.")
-    parser.add_argument("--no-ocropy", action='store_false', help="Don't perfom ocropy.")
-    parser.add_argument("--tess-profile", default='default', choices=["default"], help="Don't perfom tessract.")
-    parser.add_argument("--ocropy-profile", default='default', choices=["default"], help="Don't perfom ocropy.")
-    args = parser.parse_args()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-i", "--image",help="path to input image to be OCR'd")
+    argparser.add_argument("-f", "--imageformat", default="jpg",help="format of the images")
+    argparser.add_argument("-p", "--preprocess", type=str, default="thresh",help="type of preprocessing to be done")
+    argparser.add_argument("--no-tess", action='store_true', help="Don't perfom tessract.")
+    argparser.add_argument("--no-ocropy", action='store_false', help="Don't perfom ocropy.")
+    argparser.add_argument("--tess-profile", default='default', choices=["default"], help="Don't perfom tessract.")
+    argparser.add_argument("--ocropy-profile", default='default', choices=["default"], help="Don't perfom ocropy.")
+    args = argparser.parse_args()
     return args
 
-######### FUNCTIONS ############
+########## FUNCTIONS ##########
 def create_dir(newdir:str)->int:
     """
     Creates a new directory
@@ -68,7 +69,7 @@ def get_profiles(args,config):
             ocropy_profile = json.load(file)
     return (tess_profile,ocropy_profile)
 
-def start_tess(file,fop, profile):
+def start_tess(file,path_out, tess_profile):
     """
     Start tesseract over "pytesseract" a cli-module
     :param file: fileinputpath
@@ -76,16 +77,14 @@ def start_tess(file,fop, profile):
     :param profile: contains user-specific parameters/option for tesseract
     :return:
     """
-    create_dir(fop)
-    # load the image as a PIL/Pillow image, apply OCR, and then delete
-    # the temporary file
+    create_dir(path_out)
     parameters = ""
-    for param in profile['parameters']:
-        if profile['parameters'][param]['value'] != "" and profile['parameters'][param]['value'] != "False":
-            parameters += param+" "+profile['parameters'][param]['value']+" "
+    for param in tess_profile['parameters']:
+        if tess_profile['parameters'][param]['value'] != "" and tess_profile['parameters'][param]['value'] != "False":
+            parameters += param+" "+tess_profile['parameters'][param]['value']+" "
     parameters.strip()
     text = pytesseract.image_to_string(Image.open(file), config=parameters)
-    with open(fop + file.split('/')[-1] + '.txt', 'w') as output:
+    with open(path_out + file.split('/')[-1] + '.txt', 'w') as output:
         output.write(text)
         # show the output images
         # cv2.imshow("Image", file)
@@ -93,7 +92,7 @@ def start_tess(file,fop, profile):
         # cv2.waitKey(0)
     return 0
 
-def start_ocropy(file,fop, profile):
+def start_ocropy(file,path_out, ocropy_profile):
     """
     Start tesseract over a cli
     :param file: fileinputpath
@@ -102,49 +101,53 @@ def start_ocropy(file,fop, profile):
     :return:
     """
     fname = file.split('/')[-1].split('.')[0]
-    create_dir(fop)
-    subprocess.Popen(args=["ocropus-nlbin",file,"-o"+fop+fname+"/"]).wait()
-    subprocess.Popen(args=["ocropus-gpageseg",fop+fname+"/????.bin.png"]).wait()
-    subprocess.Popen(args=["ocropus-rpred","-Q 4",fop+fname+"/????/??????.bin.png"]).wait()
-    subprocess.Popen(args=["ocropus-hocr",fop+fname+"/????.bin.png","-o"+fop+"/"+file.split('/')[-1]+".html"]).wait()
+    create_dir(path_out)
+    subprocess.Popen(args=["ocropus-nlbin",file,"-o"+path_out+fname+"/"]).wait()
+    subprocess.Popen(args=["ocropus-gpageseg",path_out+fname+"/????.bin.png"]).wait()
+    subprocess.Popen(args=["ocropus-rpred","-Q 4",path_out+fname+"/????/??????.bin.png"]).wait()
+    subprocess.Popen(args=["ocropus-hocr",path_out+fname+"/????.bin.png","-o"+path_out+"/"+file.split('/')[-1]+".html"]).wait()
     return 0
 
-
-################ START ################
-if __name__ == "__main__":
-    """
-    Entrypoint: Searches for the files and parse them into the mainfunction (can be multiprocessed)
-    """
+########### MAIN ##########
+def start_mocrin():
     # The filespath are stored in the config.ini file.
     # And can be changed there.
     config = configparser.ConfigParser()
     config.sections()
     config.read('config.ini')
     args = get_args()
-    pathx = config['DEFAULT']['allpath']
-    pathxoutput = config['DEFAULT']['allpath_output']
+    PATHINPUT = config['DEFAULT']['PATHINPUT']
+    PATHOUTPUT = config['DEFAULT']['PATHOUTPUT']
     tess_profile, ocropy_profile = get_profiles(args, config)
-    for filen in ['short','long']:
-        for i in [1957,1961,1965,1969,1973,1976]:
-            path = pathx+filen+"/"+str(i)+"/*"
-            pathoutput = pathxoutput + filen + "/" + str(i) + "/"
-            # Get all filenames and companynames
-            files = glob2.glob(path)
-            for idx, file in enumerate(files):
-                procs = []
-                if not args.no_tess:
-                    p1 = Process(target=start_tess, args=[file, pathoutput + "tess/",tess_profile])
-                    print("Starts with Tesseract!")
-                    p1.start()
-                    procs.append(p1)
-                if not args.no_ocropy:
-                    p2 = Process(target=start_ocropy, args=[file, pathoutput + "ocropy/",ocropy_profile])
-                    print("Starts with Ocropus!")
-                    p2.start()
-                    procs.append(p2)
-                for p in procs:
-                    p.join()
-                print("Next image!")
-                #start_tess(file,pathoutput+ "tess/"))
-                #start_ocropy(file,pathoutput+ "ocropy/")
-    print("FINISHED!")
+    # Get all filenames and companynames
+    files = glob2.glob(PATHINPUT + "**/*." + args.imageformat, recursive=True)
+    for file in files:
+        path_in = file.replace(PATHINPUT, "").split("/")
+        path_out = PATHOUTPUT
+        for pathparts in path_in[:-1]:
+            path_out += pathparts + "/"
+        # Start the ocr-processes ("p") asynchronously
+        procs = []
+        if not args.no_tess:
+            p1 = Process(target=start_tess, args=[file, path_out + "tess/", tess_profile])
+            print("Call tesseract!")
+            p1.start()
+            procs.append(p1)
+        if not args.no_ocropy:
+            p2 = Process(target=start_ocropy, args=[file, path_out + "ocropy/", ocropy_profile])
+            print("Call ocropy!")
+            p2.start()
+            procs.append(p2)
+        # Wait till all ocr-processes are finished
+        for p in procs:
+            p.join()
+        print("Next image!")
+    print("All images are procesed!")
+    return 0
+
+########## START ##########
+if __name__ == "__main__":
+    """
+    Entrypoint: Searches for the files and parse them into the mainfunction (can be multiprocessed)
+    """
+    start_mocrin()
