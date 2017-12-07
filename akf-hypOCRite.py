e0884cb00cc3a8764dc7534b4f223a8ab7988f95
JKamlah <JKamlah@gmail.com>
2017-12-07 12:59:46 +0100

    Added new lib hypOCRite

---
+++ akf-hypOCRite.py
@@ -0,0 +1,14 @@
+###################### INFORMATION #############################
+#           akf-hypOCRit: Hell yeah, please OCR improved textfiles evaluation
+#           It ia an lib that evaluates your OCR-textfiles
+# Program:  **akf-hypOCRit**
+# Info:     **Python 3.6**
+# Author:   **Jan Kamlah**
+# Date:     **01.12.2017**
+
+########## IMPORT ##########
+
+
+if __name__ == "__main__":
+    print("You've got evaluated!")
+
